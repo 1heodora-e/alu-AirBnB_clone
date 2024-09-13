78850fecacb86ddd5eedcecb8e66db76dc471ed0
root <s.otieno@alustudent.com>
2024-09-13 09:59:15 -0700

    AirBnB_clone

--- console.py
+++ console.py
@@ -11,7 +11,7 @@ from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
-from models.review import eview
+from models.review import Review
 
 
 def parse(arg):
